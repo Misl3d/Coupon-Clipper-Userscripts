4d019a849fe579ce6b3946ee4ad97f0347de375c
Patryk Ziemba <55099930+Misl3d@users.noreply.github.com>
2024-08-18 13:02:49 -0400

    Create ShopriteClipper.user.js

---
+++ ShopriteClipper.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @name         Shoprite Clipper
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-18
+// @description  Clip shot Rite coupons
+// @author       Misl3d
+// @match        *://www.shoprite.com/sm/planning/rsid/538/digital-coupon
+// @match        *://shop-rite-web-prod.azurewebsites.net/*
+// @grant        none
+// ==/UserScript==
+
+// jshint esversion: 6
+
+function runSelect(event) {
+  event.preventDefault();
+
+  // Click on every "load to card" button.
+  var load2crd = document.getElementsByClassName('available-to-clip');
+  console.log(load2crd.length + ' coupons found');
+  var clicked = 0;
+
+  // Iterate in reverse because clicking on a button mutates the coupon list.
+  for (var btn of Array.from(load2crd).reverse()) {
+    btn.click();
+    clicked++;
+  }
+  console.log(clicked + ' coupons clicked');
+}
+
+function insertButton(btn) {
+  function waitForSite() {
+    var targetelem = document.getElementsByClassName('coupon-app');
+    if (targetelem !== null && targetelem[0] !== undefined) {
+      clearInterval(waitForSiteTimer);
+      targetelem[0].parentNode.insertBefore(btn, targetelem[0]);
+    }
+  }
+
+  // Wait for site to finish loading before inserting button.
+  var waitForSiteTimer = setInterval(waitForSite, 100);
+}
+
+function init() {
+  // Make a new button for our action.
+  var newbutton = document.createElement('button');
+  newbutton.name = 'load_all_to_card';
+  newbutton.id = 'load_all_to_card';
+  newbutton.style.cssText = 'background-color: #fff; color: #E82A24; font-weight: 700; border: solid #E82A24; padding: 6px 10px; cursor: pointer; margin: 5px';
+
+  newbutton.addEventListener('mouseenter',
+    () => {
+      newbutton.style.color = '#fff';
+      newbutton.style.backgroundColor = '#E82A24';
+    }
+  );
+
+  newbutton.addEventListener('mouseleave',
+    () => {
+      newbutton.style.color = '#E82A24';
+      newbutton.style.backgroundColor = '#fff';
+    }
+  );
+
+  newbutton.appendChild(document.createTextNode('Load All To Card'));
+  newbutton.addEventListener('click', runSelect);
+
+  insertButton(newbutton);
+}
+
+init();
+
+// -- The End --
